b7cd193dabe148471508d2f6588f4b2203ebb6a9
BrianXYZ <brianchima22@gmail.com>
2025-07-02 09:52:34 +0100

    implemented server guest cart route and logic

--- luxe-app-server/cart-service/index.js
+++ luxe-app-server/cart-service/index.js
@@ -6,9 +6,12 @@ const mongoose = require("mongoose");
 const dbUrl = process.env.DB_URL;
 
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
 const catchAsync = require("../shared/utlis/catchAsync.js");
 const User = require("../shared/database/models/user.js");
+const GuestUser = require("./models/guestUser.js");
+const {v4: uuidv4} = require("uuid");
 
 const PORT = process.env.PORT;
 const app = express();
@@ -25,21 +28,17 @@ const db = mongoose.connection;
 app.use(express.json());
 app.use(cors());
 
-app.put(
-  "/:userId/shoppingCart/update",
-  catchAsync(async (req, res) => {
-    const { userId } = req.params;
-    
-    const item = req.body;    
-    const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ message: "User Not Found" });
-    if (user.cart.length > 0 && user.cart.some(product => product.productId == item.productId)) {
+const updateCart = (user, item) => {
+  if (
+    user.cart.length > 0 &&
+    user.cart.some((product) => product.productId == item.productId)
+  ) {
       const newCart = user.cart.map((product) => {
         if (product.productId == item.productId) {
           if (item.units < 1) {
             product.units += 1;
             return product;
-          } else if(item.units >= 1) {
+          } else if (item.units >= 1) {
             product.units = item.units;
             return product;
           }
@@ -47,34 +46,89 @@ app.put(
           return product;
         }
       });
-      user.cart = newCart;      
-      await user.save();
-      return res.status(200).json({ cart: user.cart, message: "Added To Cart" });
+
+      user.cart = newCart;
+      return user;
     }
 
-    user.cart.push({...item, units: 1});
-    
-    await user.save();
+    user.cart.push({ ...item, units: 1 });
+    return user;
+}
 
-    res.status(200).json({ cart: user.cart, message: "Added To Cart" });
+app.put(
+  "/shoppingCart/:userId/update",
+  catchAsync(async (req, res) => {
+    const { userId } = req.params;
+
+    const item = req.body;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User Not Found" });
+    const updatedUser = updateCart(user, item)
+
+    await updatedUser.save();
+
+    res.status(200).json({ cart: updatedUser.cart, message: "Added To Cart" });
   })
 );
 
+app.put("/shoppingCart/guest/:guestId/update", catchAsync(async (req, res) => {
+  const { guestId } = req.params;
+  console.log(guestId);
+  
+  const item = req.body;
+  if(guestId === "null") {
+    const newGuest = new GuestUser({ guestId: `guest_${uuidv4()}`, cart: [{...item, units: 1}] });
+    
+    const guest = await newGuest.save().catch((err) => console.log(err));
+    
+    return res.status(200).json({ id: guest._id, cart: guest.cart, message: "Added To Cart" });
+
+  }
+  const guest = await GuestUser.findById(guestId);
+  const updatedGuest = updateCart(guest, item);
+
+  await updatedGuest.save();
+
+  res.status(200).json({ cart: updatedGuest.cart, message: "Added To Cart" });
+}));
+
 app.put(
-  "/:userId/shoppingCart/delete",
+  "/shoppingCart/:userId/delete",
   catchAsync(async (req, res) => {
     const { userId } = req.params;
     const { itemId } = req.query;
-    
+
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User Not Found" });
-    const updatedCart = user.cart.filter(product => product.productId != itemId)
-    
+    const updatedCart = user.cart.filter(
+      (product) => product.productId != itemId
+    );
+
     user.cart = updatedCart;
 
     await user.save();
 
     res.status(200).json({ cart: user.cart, message: "Removed From Cart" });
+  })
+);
+
+app.put(
+  "/shoppingCart/guest/:guestId/delete",
+  catchAsync(async (req, res) => {
+    const { guestId } = req.params;
+    const { itemId } = req.query;
+
+    const guest = await GuestUser.findById(guestId);
+    if (!guest) return res.status(404).json({ message: "Guest Cart Not Found" });
+    const updatedCart = guest.cart.filter(
+      (product) => product.productId != itemId
+    );
+
+    guest.cart = updatedCart;
+
+    await guest.save();
+
+    res.status(200).json({ cart: guest.cart, message: "Removed From Cart" });
   })
 );
 
