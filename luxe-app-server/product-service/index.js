5f25122b094c8ef35bfd506c455ba8721c86f4a8
BrianXYZ <brianchima22@gmail.com>
2025-07-10 23:55:48 +0100

    created product service, moved product crud route and product model fron inventory to product service

---
+++ luxe-app-server/product-service/index.js
@@ -0,0 +1,53 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+const cors = require("cors");
+const catchAsync = require("../shared/utlis/catchAsync.js");
+const dbUrl = process.env.DB_URL;
+const port = process.env.PORT;
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+
+  db.on("error", console.error.bind(console, "Connection error:"));
+  db.once("open", () => {
+    console.log("Database Connected");
+  });
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+const pageLimit = 10;
+
+app.get("/products", async (req, res) => {
+  const {page} = req.query;
+  
+  const skip = (page - 1) * pageLimit;
+  
+  const product = await Product.find().skip(skip).limit(pageLimit);
+
+  res.json(product);
+});
+
+
+
+app.get("/products/:productId", async (req, res) => {
+  const {productId} = req.params;
+    console.log(productId);
+    
+  const product = await Product.findById(productId);
+
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log("Connected and listening on port " + port);
+});
+  
