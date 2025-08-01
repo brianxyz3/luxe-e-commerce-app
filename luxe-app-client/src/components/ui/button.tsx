deff5b86b83966e7becf3d7258fa2660c84477d4
BrianXYZ <brianchima22@gmail.com>
2025-08-01 10:33:18 +0100

    imported component from shadcn to use in inventory edit form

--- luxe-app-client/src/components/ui/button.tsx
+++ luxe-app-client/src/components/ui/button.tsx
@@ -5,20 +5,19 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm lg:text-lg font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm lg:text-lg font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:cursor-pointer",
   {
     variants: {
       variant: {
         default:
-          "bg-amber-500 text-primary-foreground shadow hover:bg-amber-500/90",
+          "bg-amber-500 dark:bg-amber-500/80 text-secondary-foreground shadow hover:bg-amber-500/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-blue-600 text-secondary-foreground shadow-sm hover:bg-blue-600/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
