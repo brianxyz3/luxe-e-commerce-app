207f0b5b73576cd3fffc5ce696361cf5589c7545
BrianXYZ <brianchima22@gmail.com>
2025-08-07 18:51:11 +0100

    changed login link icon for better user experience

--- luxe-app-client/src/components/HeaderNav.tsx
+++ luxe-app-client/src/components/HeaderNav.tsx
@@ -1,15 +1,17 @@
 import { Input } from "@/components/ui/input";
-import { Boxes, LogOut, MoonStar, Plus, Search, ShoppingCart, Sun, User, UserCheck } from "lucide-react";
+import { Boxes, LogInIcon, LogOut, MoonStar, Plus, Search, ShoppingCart, Sun, User, UserCheck } from "lucide-react";
 import { heroModelImg } from "@/assets/images";
-import { Link, NavLink, useNavigate } from "react-router";
-import React, { useRef, useState } from "react";
+import { Link, NavLink } from "react-router";
+import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useTheme } from "@/context/themeContext";
 import { useAuth } from "@/context/authContext";
 import { useCart } from "@/context/cartContext";
-import { signOut } from "@/controller/auth";
-import { toast } from "react-toastify";
+// import { signOut } from "@/controller/auth";
+// import { toast } from "react-toastify";
+import { Button } from "./ui/button";
+import type { ProductType } from "@/types";
 
 gsap.registerPlugin(useGSAP);
 
@@ -17,11 +19,11 @@ const HeaderNav: React.FC<{isHome?: boolean}>  = ({isHome = false}) => {
 
   const [showNavBar, setShowNavBar] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState<ProductType[] | undefined>([]);
   const {theme, toggleTheme} = useTheme();
   const navBtn = useRef(null);
   const {userLoggedIn, handleLogInState} = useAuth();
   const {cart} = useCart();
-  const navigate = useNavigate();
 
   const navBtnTL = gsap.timeline({
     defaults: {
@@ -65,6 +67,20 @@ const HeaderNav: React.FC<{isHome?: boolean}>  = ({isHome = false}) => {
     setShowNavBar(prevState => !prevState)
   });
 
+
+  const handleSearch = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    const {productList} = useProductFetch( 1, searchQuery);
+    if(!productList) {
+      setSearchResult(undefined)
+    } else {
+      setSearchResult([...productList])
+    }
+  }
+  useEffect(() => {
+    if(!Boolean(searchQuery.trimStart())) return setSearchResult(undefined)
+  }, [searchQuery])
+
   return (
     <header className="dark:bg-black">
       <section className="fixed backdrop-blur-md bg-white/15 dark:bg-amber-900/15  w-full md:sticky z-50 top-0 shadow-sm p-4 flex items-center justify-between">
@@ -81,15 +97,26 @@ const HeaderNav: React.FC<{isHome?: boolean}>  = ({isHome = false}) => {
 
         <NavLink to="/" className="text-3xl text-right sm:text-left w-1/3 sm:w-fit font-bold mx-auto md:m-0 text-amber-600">LUXÉ</NavLink>
         
-        {isHome && <div className="w-1/2 hidden md:flex">
-          <div className="relative flex w-full">
+        {isHome && <div className="w-1/2 hidden md:flex flex-col">
+          <div className="h-fit relative flex w-full">
             <Input
               placeholder="Search products..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="pl-10 rounded-full w-full"
+              className="pl-10 rounded-full w-full bg-cream-lighter"
             />
             <Search className="absolute top-2.5 left-3 w-5 h-5 text-gray-500" />
+          </div>
+          <div className={`${searchQuery ? "h-56" : "h-0"} rounded-t-none rounded-2xl overflow-auto bg-cream-lighter w-2/5 flex flex-col fixed top-14 -translate-x-1/2 left-1/2 duration-300`}>
+            {
+              searchResult ? searchResult.map(product => (
+                <Link
+                 key={product._id}
+                  to={`/products/${product._id}`}
+                  className="mt-3 mx-4 font-semibold tracking-wider">{product.name}</Link>
+              ))
+              : <p>No Product Found</p>
+            }
           </div>
         </div>}
         <div className="flex items-center  text-gray-700 dark:text-white gap-3 md:gap-4">
@@ -99,15 +126,15 @@ const HeaderNav: React.FC<{isHome?: boolean}>  = ({isHome = false}) => {
               toggleTheme("light");
             }}
             title="toggle theme" type="button" className="relative">
-            <MoonStar className={`${theme != "dark" && "opacity-0"} size-5 md:size-7 aspect-square duration-300 fixed`}/>
-            <Sun className={`${theme == "dark" && "opacity-0"} size-5 md:size-7 aspect-square duration-300`}/>
+            <MoonStar className={`${theme == "dark" && "opacity-0"} size-5 md:size-7 aspect-square duration-300 fixed`}/>
+            <Sun className={`${theme != "dark" && "opacity-0"} size-5 md:size-7 aspect-square duration-300`}/>
           </button>
           <Link to="/shoppingCart"
             className="relative">
               {cart.length > 0 && <div className="w-3 md:w-4 aspect-square absolute -top-3 -z-10 right-0.5 md:right-1 rounded-full font-bold text-sm flex items-center justify-center"><Boxes className="fill-amber-500 dark:stroke-amber-600 dark:fill-transparent"/></div>}
               <ShoppingCart className="size-5 md:size-7 aspect-square" />
           </Link>
-          <Link className={`${userLoggedIn && "hidden"}`} to="/auth"><User className="size-5 md:size-7 aspect-square" /></Link>
+          <Link className={`${userLoggedIn && "hidden"}`} to="/auth"><LogInIcon className="size-5 md:size-7 aspect-square" /></Link>
           <Link className={`${!userLoggedIn && "hidden"}`} to="/dashboard"><UserCheck className="size-5 md:size-7 aspect-square" /></Link>
           {/* <button title="Log out" type="button" className={`${!userLoggedIn && "hidden"}`} 
           onClick={async () => {
@@ -124,9 +151,9 @@ const HeaderNav: React.FC<{isHome?: boolean}>  = ({isHome = false}) => {
       {/* Navigation Menu */}
       <nav className="hidden bg-cream-darker md:px-6 py-2 text-sm font-medium text-gray-800 md:flex justify-center gap-5">
       <NavLink to="/" key={"home"} className="hover:text-amber-300 text-nowrap transition-colors">Home</NavLink>
-      <NavLink to="/products" key={"shop-all"} className="hover:text-amber-300 text-nowrap transition-colors">Shop All</NavLink>
+      <NavLink to="/products?category=all" key={"shop-all"} className="hover:text-amber-300 text-nowrap transition-colors">Shop All</NavLink>
         {["Skin-care", "Hair-care", "Makeup", "Fragrance", "Gifts", "Sale"].map((item) => (
-          <NavLink to={`/products/search?category=${item.toLocaleLowerCase()}`} key={item} className="hover:text-amber-300 text-nowrap transition-colors">{item}</NavLink>
+          <NavLink to={`/products?category=${item.toLocaleLowerCase()}`} key={item} className="hover:text-amber-300 text-nowrap transition-colors">{item}</NavLink>
         ))}
         <NavLink to="/about" key={"about"} className="hover:text-amber-300 text-nowrap transition-colors">About Us</NavLink>
       </nav>
@@ -134,20 +161,36 @@ const HeaderNav: React.FC<{isHome?: boolean}>  = ({isHome = false}) => {
       {/* Mobile Navigation Menu */} 
       <section className={`grid grid-cols-1 h-dvh w-dvw md:hidden bg-white dark:bg-stone-600 overflow-hidden z-20 fixed inset-0 duration-500 ease-in ${!showNavBar && "-left-full"}`}>
         <div className="w-4/5 mx-auto mt-[5rem] flex md:hidden">
-          <div className="relative flex w-full rounded-lg bg-gray-300">
+          <form onSubmit={handleSearch} className="relative flex w-full rounded-lg bg-gray-300 dark:bg-stone-500">
             <Input
               placeholder="Search products..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="pl-10 rounded-lg dark:text-black"
+              className="rounded-r-none pl-10 rounded-lg dark:text-black"
             />
-            <Search className="absolute top-2.5 left-3 w-5 h-5 text-gray-500 dark:text-cream-darker" />
+            <Button type="submit" className="rounded-l-none">Search</Button>
+            <Search className="absolute top-2.5 left-3 w-5 h-5 text-gray-500 dark:text-stone-100" />
+          </form>
+          <div className={`${searchQuery ? "h-1/2" : "h-0"} rounded-t-none rounded-2xl overflow-auto bg-cream-lighter w-full z-10 flex flex-col fixed top-32 -translate-x-1/2 left-1/2 duration-300`}>
+            {
+              searchResult ? searchResult.map(product => (
+                <Link
+                 key={product._id}
+                  to={`/products/${product._id}`}
+                  onClick={() => {
+                    toggleNavBar()
+                    setSearchQuery("")
+                  }}
+                  className="mt-3 mx-4 font-semibold tracking-wider">{product.name}</Link>
+              ))
+              : <p className="mx-auto">No Product Found</p>
+            }
           </div>
         </div>
         <nav className="pb-2 ps-6 dark:bg-stone-600">
           <div className="text-cream-darker dark:text-cream-light overflow-auto text-xl font-bold flex gap-5 px-2.5 ms-0.5 py-4">
             {["Skin-care", "Hair-care", "Makeup", "Fragrance", "Gifts", "Sale"].map((item) => (
-              <NavLink to={`/products/search?category=${item.toLocaleLowerCase()}`} key={item} onClick={toggleNavBar} className="hover:text-amber-300 scale-y-125 text-nowrap transition-colors">{item}</NavLink>
+              <NavLink to={`/products?category=${item.toLocaleLowerCase()}`} key={item} onClick={toggleNavBar} className="hover:text-amber-300 scale-y-125 text-nowrap transition-colors">{item}</NavLink>
             ))}
             <NavLink to="/about" onClick={toggleNavBar} key={"about"} className="hover:text-amber-300 text-nowrap transition-colors">About Us</NavLink>
           </div>
